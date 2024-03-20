17068cf152badd9dff8d03cd854a1a328415d77a
kokorokjo <szollosmartin@gmail.com>
2024-03-20 13:00:14 +0100

    Ulcs

--- APrograms/Alcs.cpp
+++ APrograms/Alcs.cpp
@@ -9,17 +9,13 @@ int longestCommonSubsequence(string& text1, string& text2)
 	
 	vector<int> prev(m + 1, 0), cur(m + 1, 0); 
 
-	for (int idx2 = 0; idx2 < m + 1; idx2++) 
-		cur[idx2] = 0; 
-
 	for (int idx1 = 1; idx1 < n + 1; idx1++) { 
 		for (int idx2 = 1; idx2 < m + 1; idx2++) { 
 			if (text1[idx1 - 1] == text2[idx2 - 1]) 
 				cur[idx2] = 1 + prev[idx2 - 1]; 
  
 			else
-				cur[idx2] 
-					= 0 + max(cur[idx2 - 1], prev[idx2]); 
+				cur[idx2] = 0 + max(cur[idx2 - 1], prev[idx2]); 
 		} 
 		prev = cur; 
 	} 
